9ad52584cb14966c59edc35cf30a7bf0981bf058
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-08-10 10:36:33 +0100

    Set up tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Coming Soon</h1>
+        <h1 className="text-center text-[#f00]">Coming Soon!</h1>
       </header>
     </div>
   );
