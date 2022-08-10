fc310b8df613095d5a81c14817098a793aa8eed7
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-08-10 11:43:39 +0100

    Add necessary dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
 import './index.css';
+
+const client = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </QueryClientProvider>,
 );
