8adbbe0cc9ffd2b0034a33397f8dd223476c5993
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-08-11 17:04:35 +0100

    fetch data from fakestoreapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,40 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+
+//styles
+import { Wrapper } from './App.styles';
+
+// tpyes
+export type CartItemType = {
+  id: number;
+  title: string;
+  price: number;
+  category: string;
+  description: string;
+  image: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
+  console.log(data);
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = () => null;
+
+  const handleRemoveFromCart = () => null;
+
+  isLoading && <LinearProgress />;
+  error && <div>Something went wrong! Try again</div>;
   return (
     <div className="App">
       <header className="App-header">
