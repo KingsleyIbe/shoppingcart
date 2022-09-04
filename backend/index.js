a77fc981be0ae6591d0d0bc49c0d8d7af418f535
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-09-04 13:51:35 +0100

    Add data file

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+import data from './productData';
+
 const cors = require('cors');
 const express = require('express');
 
@@ -9,4 +11,14 @@ app.get('/', (req, res) => {
   res.send('Welcome to online shopping app api');
 });
 
-app.listen(5000, console.log('Server running on port 5000'));
+app.get('/products', (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+app.get('/items', (req, res) => {
+  res.send(data);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, console.log(`Server running on port ${port}`));
